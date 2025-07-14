38c559a77f95b4c43e298afca3242dd6e808470a
alexthomp25 <alexandrianicolethompson@gmail.com>
2025-07-14 17:36:49 -0400

    Add files via upload

---
+++ info2.py
@@ -0,0 +1,7 @@
+#This File will contain the information to be displayed in phase2
+
+#HEADER FOR PAGE
+text = "Hello! I am the cabbage merchant and I am trying to sell some cabbages!"
+more_text = "Please consider buying some cabbages! They are very tasty and very nutritious!!!"
+
+
